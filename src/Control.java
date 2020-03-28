dcdd7ed6d05174a02beb0fc8b32a9ea386aba846
Dpens <514682604@qq.com>
2020-03-28 15:52:50 +0800

    inputTest completed

--- src/Control.java
+++ src/Control.java
@@ -2,18 +2,34 @@ import java.util.Scanner;
 
 
 /**
+ * @Description cat,dog Animal类
+ *              strength 攻击力度
+ *              operation 操作(0：表示选择道具的输入操作，1表示攻击力度的输入操作
+ *              over 比赛是否结束，True结束，False没结束
+ *              round 回合数
+ *              roundOwner 当前回合所有者
  * @author  Lv and Zhou
- *
+ * @date 2020.3.27
  */
 public class Control {
     private Animal cat;
     private Animal dog;
     private int strength;
+    private int operation;
+    private int operationNum;
     private boolean over;
     private int round;
+    private String roundOwner;
     public static final int MAXSTRENGTH = 10;
+    public static final int MINSTRENGTH = 0;
     public static final int MINHITSTRENGTH = 4;
     public static final int MAXHITSTRENGTH = 6;
+    public static final int TOOLOP = 1;
+    public static final int ATKOP = 2;
+    public static final int OP1LOWBORDER = 1;
+    public static final int OP1HIGHBORDER = 5;
+    public static final int OP2LOWBORDER = 0;
+    public static final int OP2HIGHBORDER = 10;
 
 
     public Control(){
@@ -24,12 +40,24 @@ public class Control {
         this.round = 1;
     }
 
+    public void setRoundOwner(String roundOwner) {
+        this.roundOwner = roundOwner;
+    }
+
     public int getRound() {
         return round;
     }
 
     public void setRound(int round) {
         this.round = round;
+    }
+
+    public void setOperation(int operation) {
+        this.operation = operation;
+    }
+
+    public void setOperationNum(int operationNum) {
+        this.operationNum = operationNum;
     }
 
     public boolean getOver() {
@@ -67,7 +95,7 @@ public class Control {
 
         System.out.println("猫猫的回合，请输入猫猫的攻击力度(0~10): ");
         int strength = sc.nextInt();
-        while (strength < 0 || strength > MAXSTRENGTH){
+        while (strength < MINSTRENGTH || strength > MAXSTRENGTH){
             System.out.println("攻击力度有误，只能输入0~10");
             strength=sc.nextInt();
         }
@@ -87,33 +115,61 @@ public class Control {
         }
     }
 
-    public void getInput(){
-
+    public int getInput(){
+//        Scanner scanner= new Scanner(System.in);
+        if(this.operation == TOOLOP){
+            System.out.println("请选择你的操作（输入操作前的数字）");
+            System.out.println("1.不使用道具");
+            System.out.println("2.使用道具1");
+            System.out.println("3.使用道具2");
+            System.out.println("4.使用道具3");
+            System.out.println("5.使用道具4");
+//            this.setOperationNum(scanner.nextInt());
+            if(this.operationNum <= OP1HIGHBORDER && this.operationNum >= OP1LOWBORDER){
+                this.setOperation(ATKOP);
+                return this.operationNum;
+            }
+        }
+        else if(this.operation == ATKOP){
+            System.out.println("请输入你的攻击力度（0-10）");
+//            this.setOperationNum(scanner.nextInt());
+            if(this.operationNum <= OP2HIGHBORDER && this.operationNum >= OP2LOWBORDER){
+                this.setOperation(TOOLOP);
+                return this.operationNum;
+            }
+        }
+        System.out.println("输入不在正确范围内，请重新输入");
+        return -1;
     }
 
-    public void useTool(){}
+    public int useTool(){
+        if(cat.getName().equals(this.roundOwner)){
+            return cat.useTool();
+        }else{
+            return dog.useTool();
+        }
+    }
 
 
     public static void main(String[] args){
-        Control control = new Control();
-        Scanner sc = new Scanner(System.in);
-        while (true){
-            System.out.println("ROUND  " + control.getRound());
-            control.setRound(control.getRound()+1);
-            control.output();
-            control.animalAtk(sc);
-            control.output();
-            if(control.getOver()){
-                System.out.println("Cat WIN");
-                break;
-            }
-            control.animalAtk(sc);
-            control.output();
-            if(control.getOver()){
-                System.out.println("Dog WIN");
-                break;
-            }
-            System.out.println("==========================================================");
-        }
+//        Scanner sc = new Scanner(System.in);
+//        while (true){
+//            System.out.println("ROUND  " + control.getRound());
+//            control.setRound(control.getRound()+1);
+//            control.output();
+//            control.animalAtk(sc);
+//            control.output();
+//            if(control.getOver()){
+//                System.out.println("Cat WIN");
+//                break;
+//            }
+//            control.animalAtk(sc);
+//            control.output();
+//            if(control.getOver()){
+//                System.out.println("Dog WIN");
+//                break;
+//            }
+//            System.out.println("==========================================================");
+//        }
     }
 }
--- test/ControlTest.java
+++ test/ControlTest.java
@@ -1,22 +1,87 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
+/**
+ * @author Zhou
+ *
+ */
+@RunWith(Parameterized.class)
 public class ControlTest {
+    public ControlTest controlTest;
+    private int expected;
+    private int input1;
+    private int input2;
+
+
+    public ControlTest(int expected, int input1, int input2){
+        this.expected = expected;
+        this.input1 = input1;
+        this.input2 = input2;
+    }
+
+    @Parameterized.Parameters(name="{index}:getArea[{1},{2}]={0}")
+    public static Collection testData(){
+        return Arrays.asList(new Object[][]{
+                {-1, 0, 5},
+                {5, 1, 5},
+                {5, 2, 5},
+                {-1, 3, 5},
+                {-1, 0, 2},
+                {2, 1, 2},
+                {2, 2, 2},
+                {-1, 3, 2},
+                {-1, 1, 0},
+                {1, 1, 1},
+                {2, 1, 2},
+                {4, 1, 4},
+                {5, 1, 5},
+                {-1, 1, 6},
+                {-1, 2, -1},
+                {0, 2, 0},
+                {1, 2, 1},
+                {9, 2, 9},
+                {10, 2, 10},
+                {-1, 2, 11},
+        });
+    }
+
+    @Before
+    public void setUp(){
+        this.controlTest = new ControlTest(expected, input1, input2);
+    }
+
+    @After
+    public void tearDown(){
+        System.out.println("finish");
+    }
 
     @Test
     public void output() {
     }
 
     @Test
-    public void cat_ATK() {
-    }
-
-    @Test
-    public void dog_ATK() {
-    }
-
-    @Test
     public void gameOver() {
+    }
+
+    @Test
+    public void getInput() {
+        Control control = new Control();
+        control.setOperation(input1);
+        control.setOperationNum(input2);
+        int result = control.getInput();
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void useTool() {
+        fail();
     }
 }
