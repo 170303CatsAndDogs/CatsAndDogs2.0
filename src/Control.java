551125d65f4a832470a774e4d4c2789c59bf36ac
Dpens <514682604@qq.com>
2020-03-29 15:38:47 +0800

    Refactor the code and add comments to increase readability

--- src/Control.java
+++ src/Control.java
@@ -2,13 +2,23 @@ import java.util.Scanner;
 
 
 /**
- * @Description cat,dog Animal类
- *              strength 攻击力度
- *              operation 操作(0：表示选择道具的输入操作，1表示攻击力度的输入操作
- *              over 比赛是否结束，True结束，False没结束
- *              round 回合数
- *              roundOwner 当前回合所有者
- * @author  Lv and Zhou
+ * The type Control.
+ *
+ * @author Lv and Zhou
+ * @Description cat, dog Animal类
+ * strength 攻击力度
+ * operation 操作(0：表示选择道具的输入操作，1表示攻击力度的输入操作
+ * over 比赛是否结束，True结束，False没结束
+ * round 回合数
+ * roundOwner 当前回合所有者 0：cat，1：dog
+ * MAXSTRENGTH  最大输入力度
+ * MINSTRENGTH  最小输入力度
+ * MINHITSTRENGTH  最小击中力度
+ * MAXHITSTRENGTH  最大击中力度
+ * TOOLOP  表示当前回合为选择道具阶段
+ * ATKOP   表示当前回合为输入攻击力度阶段
+ * TOOLOPLOWBORDER    选择道具阶段输入的下界
+ * TOOLOPHIGHBORDER   选择道具阶段输入的上界
  * @date 2020.3.27
  */
 public class Control {
@@ -19,17 +29,15 @@ public class Control {
     private int operationNum;
     private boolean over;
     private int round;
-    private String roundOwner;
+    private int roundOwner;
     public static final int MAXSTRENGTH = 10;
     public static final int MINSTRENGTH = 0;
     public static final int MINHITSTRENGTH = 4;
     public static final int MAXHITSTRENGTH = 6;
     public static final int TOOLOP = 1;
     public static final int ATKOP = 2;
-    public static final int OP1LOWBORDER = 1;
-    public static final int OP1HIGHBORDER = 5;
-    public static final int OP2LOWBORDER = 0;
-    public static final int OP2HIGHBORDER = 10;
+    public static final int TOOLOPLOWBORDER = 1;
+    public static final int TOOLOPHIGHBORDER = 2;
 
 
     public Control(){
@@ -40,7 +48,7 @@ public class Control {
         this.round = 1;
     }
 
-    public void setRoundOwner(String roundOwner) {
+    public void setRoundOwner(int roundOwner) {
         this.roundOwner = roundOwner;
     }
 
@@ -115,17 +123,24 @@ public class Control {
         }
     }
 
+
+    /**
+     * Get input int.
+     *在一个回合中获取玩家的两次输入。
+     * @return the int
+     */
     public int getInput(){
 //        Scanner scanner= new Scanner(System.in);
         if(this.operation == TOOLOP){
             System.out.println("请选择你的操作（输入操作前的数字）");
             System.out.println("1.不使用道具");
-            System.out.println("2.使用道具1");
-            System.out.println("3.使用道具2");
-            System.out.println("4.使用道具3");
-            System.out.println("5.使用道具4");
+            System.out.println("2.使用道具");
+//            System.out.println("2.使用道具1");
+//            System.out.println("3.使用道具2");
+//            System.out.println("4.使用道具3");
+//            System.out.println("5.使用道具4");
 //            this.setOperationNum(scanner.nextInt());
-            if(this.operationNum <= OP1HIGHBORDER && this.operationNum >= OP1LOWBORDER){
+            if(this.operationNum <= TOOLOPHIGHBORDER && this.operationNum >= TOOLOPLOWBORDER){
                 this.setOperation(ATKOP);
                 return this.operationNum;
             }
@@ -133,9 +148,9 @@ public class Control {
         else if(this.operation == ATKOP){
             System.out.println("请输入你的攻击力度（0-10）");
 //            this.setOperationNum(scanner.nextInt());
-            if(this.operationNum <= OP2HIGHBORDER && this.operationNum >= OP2LOWBORDER){
+            if(this.strength <= MAXSTRENGTH && this.strength >= MINSTRENGTH){
                 this.setOperation(TOOLOP);
-                return this.operationNum;
+                return this.strength;
             }
         }
         System.out.println("输入不在正确范围内，请重新输入");
@@ -143,13 +158,32 @@ public class Control {
     }
 
     public int useTool(){
-        if(cat.getName().equals(this.roundOwner)){
-            return cat.useTool();
+        if(this.operationNum == TOOLOPHIGHBORDER){
+            if(this.roundOwner == 0){
+                return cat.useTool();
+            }else if(this.roundOwner == 1){
+                return dog.useTool();
+            }else{
+                return -1;
+            }
         }else{
-            return dog.useTool();
+            return -1;
         }
     }
 
+    public void changeRound(){
+        this.setRoundOwner((this.roundOwner + 1) % 2);
+    }
+
+    public void setInput(int input){
+        if(this.operation == TOOLOP){
+            this.setOperationNum(input);
+        }else if(this.operation == ATKOP){
+            this.setStrength(input);
+        }else{
+            return;
+        }
+    }
 
     public static void main(String[] args){
 //        Scanner sc = new Scanner(System.in);
