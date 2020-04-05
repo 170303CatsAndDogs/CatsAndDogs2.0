3cae24eaf9e2e451b05c978cc625bba3d3195599
stwalker <casdgh123@hotmail.com>
2020-04-05 14:06:45 +0800

    improve code quality

--- src/Wind.java
+++ src/Wind.java
@@ -5,19 +5,30 @@ import java.util.Random;
  */
 public class Wind {
 
-    // 风向(1：顺风，-1：逆风)
+    /**
+     * 风向(1：顺风，-1：逆风)
+     */
     private String direct;
-    // 风力(范围0-10)
+    /**
+     * 风力(范围0-3，等于0时表示无风)
+     */
     private int windStrength;
-    // 产生随机数
+
     private Random random;
 
-    // Constructors
+    public static final String [] DIRECTIONS = { "顺风", "逆风", "无风" };
+
+
+    /**
+     * Constructors
+     */
     public Wind () {
         getRandomWind();
     }
 
-    // Getters and Setters
+    /**
+     * Getters and Setters
+     */
     public String getDirect() {
         return direct;
     }
@@ -37,19 +48,19 @@ public class Wind {
 
     /**
      * 得到随机风力和风向
-     * 风力大小：0-4，风向：顺风、逆风，无风
+     * 风力大小：0-3，风向：顺风、逆风，无风
      */
     public void getRandomWind() {
         random = new Random();
         windStrength = random.nextInt(4);
         int dir = random.nextInt(2);
         if (dir == 0) {
-            this.direct = "顺风";
+            this.direct = DIRECTIONS[0];
         }else {
-            this.direct = "逆风";
+            this.direct = DIRECTIONS[1];
         }
-        if(windStrength == 0){
-            this.direct = "无风";
+        if (windStrength == 0) {
+            this.direct = DIRECTIONS[2];
         }
     }
 
@@ -57,13 +68,13 @@ public class Wind {
      * @return 风力值顺风为正、逆风为负
      */
     public int getActualWindStrength() {
-        if ("顺风".equals(this.direct)) {
+        if (DIRECTIONS[0].equals(this.direct)) {
             return this.windStrength;
         }
-        if ("逆风".equals(this.direct)) {
+        if (DIRECTIONS[1].equals(this.direct)) {
             return -this.windStrength;
         }
-        if ("无风".equals(this.direct)) {
+        if (DIRECTIONS[2].equals(this.direct)) {
             return this.windStrength;
         }
         return -1;
