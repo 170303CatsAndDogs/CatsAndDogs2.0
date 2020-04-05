8abc9ac3239f4102ab35aa83ad57a0e9f1578aeb
Dpens <514682604@qq.com>
2020-04-05 13:40:23 +0800

    "mod ControlTestSuite"

--- test/ControlTestSuite.java
+++ test/ControlTestSuite.java
@@ -2,6 +2,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ControlGetInputTest.class, ControlUseToolTest.class})
+@Suite.SuiteClasses({ControlGetInputTest.class, ControlUseToolTest.class, AnimalTest.class,
+        ControlTestAttack.class,ControlTestIsNumeric.class,
+        ControlWhoGoesFirstJudgeTest.class, WindTest.class
+})
 public class ControlTestSuite {
 }
