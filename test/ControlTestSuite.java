085094e45e72bad04865ade32dc6d8a0bd426bef
Dpens <514682604@qq.com>
2020-04-05 10:39:54 +0800

    "null"

--- test/ControlTestSuite.java
+++ test/ControlTestSuite.java
@@ -2,6 +2,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ControlTest.class, ControlUseToolTest.class})
+@Suite.SuiteClasses({ControlGetInputTest.class, ControlUseToolTest.class})
 public class ControlTestSuite {
 }
