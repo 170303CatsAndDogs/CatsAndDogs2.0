24efe64bae40cb8a0eab99f948a45ec4a55db729
Dpens <514682604@qq.com>
2020-04-05 13:40:48 +0800

    "mod ControlUserToolTest"

--- test/ControlUseToolTest.java
+++ test/ControlUseToolTest.java
@@ -14,33 +14,28 @@ import static org.junit.Assert.assertEquals;
 public class ControlUseToolTest {
     public ControlUseToolTest controlUseToolTest;
     private int expected;
-    private int roundOwner;
     private int operationNum;
 
 
-    public ControlUseToolTest(int expected, int roundOwner, int operationNum){
+    public ControlUseToolTest(int expected, int operationNum){
         this.expected = expected;
-        this.roundOwner = roundOwner;
         this.operationNum = operationNum;
     }
 
-    @Parameterized.Parameters(name="{index}:useTool[{1},{2}]={0}")
+    @Parameterized.Parameters(name="{index}:useTool[{1}]={0}")
     public static Collection testData(){
         return Arrays.asList(new Object[][]{
-                {-1, 0, 0},
-                {-1, 0, 1},
-                {0, 0, 2},
-                {-1, 0, 3},
-                {-1, -1, 2},
-                {0, 0, 2},
-                {0, 1, 2},
-                {-1, 2, 2},
+                {-1, 1},
+                {0, 4},
+                {0, 5},
+                {-1, 6},
+                {0, 3},
         });
     }
 
     @Before
     public void setUp(){
-        this.controlUseToolTest = new ControlUseToolTest(expected, roundOwner, operationNum);
+        this.controlUseToolTest = new ControlUseToolTest(expected, operationNum);
     }
 
     @After
@@ -51,7 +46,6 @@ public class ControlUseToolTest {
     @Test
     public void useTool() {
         Control control = new Control();
-        control.setRoundOwner(roundOwner);
         control.setOperationNum(operationNum);
         assertEquals(expected, control.useTool());
     }
